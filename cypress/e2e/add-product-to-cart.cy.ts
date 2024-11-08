7a96c456eb730de3e855a2cdedd3da7ccbcaf888
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-08 07:56:30 -0300

    testes end2end carrinho

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,25 @@
+describe('add product to cart', () => {
+  it('should to be able to navigate to the product page and add it to the cart', () => {
+    cy.visit("http://localhost:3000")
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+  })
+  it('should not be able to add the same product twice', () => {
+    cy.visit("http://localhost:3000")
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+  })
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.visit("http://localhost:3000")
+    cy.get('input[name="q"]').type('camiseta').parent("form").submit()
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+  })
+})
