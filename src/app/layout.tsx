eaf066aa396e1ff659e3a44330cdeda3ae2a1eb5
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-04-23 02:14:38 -0300

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -24,6 +25,7 @@ export default function RootLayout({
         className="bg-zinc-950 text-zinc-50 antialiased"
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
